5146ff6c55b0cbed5f59b89ed92bc4cca86cf313
Serhan Yılmaz <srhnylmz14@gmail.com>
2023-06-26 17:21:14 +0200

    delete team.js

--- team.js
+++
@@ -1,6 +0,0 @@
-// Description: JavaScript for team.html
-document.querySelectorAll('.team-member img').forEach(img => {
-    img.addEventListener('click', function () {
-        window.open(this.dataset.url, '_blank');
-    });
-});
